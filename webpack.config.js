39ecbac6c5df01d57274cb68fdade70cd09bea5f
zbyslav ostapiak <67975727+zbys1av@users.noreply.github.com>
2023-07-07 03:32:19 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist")
+    },
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new CopyWebpackPlugin({'patterns': [
+        {from:'./src/images', to:'images'}
+      ]}),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
